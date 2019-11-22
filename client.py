430c465c80020ac1ce4a5d1147f51411121aad9f
dz4485 <daniel.zamir@mail.huji.ac.il>
2019-11-22 08:52:33 +0200

    Client now also returns thumbnail of image, and parses out output.

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import get_ids_from_folder
-from  app import guess_the_image_content
+from app import guess_the_image_content
 
 client = Flask(__name__)
 
@@ -8,8 +8,10 @@ client = Flask(__name__)
 @client.route('/')
 def home():
     image_id = get_ids_from_folder.get_ids('1W9QPPgnXqJkY4pk0Wcx0m427JNT1Hwer')[0]
-    res = f'First file in folder "1W9QPPgnXqJkY4pk0Wcx0m427JNT1Hwer": {image_id}'
-    res = res + f'The app will now try to guess the conent of the image: {guess_the_image_content(image_id)}'
+    res = f'<!DOCTYPE html><html><body>First file in folder "1W9QPPgnXqJkY4pk0Wcx0m427JNT1Hwer": "{image_id}"<br>'
+    res = res + f'The app will now try to guess the content of the image: <br>{guess_the_image_content(image_id)}<br>'
+    res = res + f'<img src="https://drive.google.com/thumbnail?id={image_id}' \
+                f'&sz=w400-h300-p-k-nu" alt="Image" height="100" width="100"></body></html>'
 
     return res
     # return 'hello world!'
